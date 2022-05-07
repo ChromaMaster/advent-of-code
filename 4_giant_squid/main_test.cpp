73e13b7f3cd860a9c669534030f31b6f62222537
ChromaMaster <vladtel.dev@gmail.com>
2022-05-07 15:50:17 +0200

    feat: add day four exercise

---
+++ 4_giant_squid/main_test.cpp
@@ -0,0 +1,25 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+
+#include "doctest.h"
+
+int main(int argc, char **argv) {
+    doctest::Context context;
+
+    // !!! THIS IS JUST AN EXAMPLE SHOWING HOW DEFAULTS/OVERRIDES ARE SET !!!
+
+    // defaults
+    context.setOption("abort-after", 5);              // stop test execution after 5 failed assertions
+    context.setOption("order-by", "name");            // sort the test cases by their name
+
+    context.applyCommandLine(argc, argv);
+
+    // overrides
+    context.setOption("no-breaks", true);             // don't break in the debugger when assertions fail
+
+    int res = context.run(); // run
+
+    if (context.shouldExit()) { // important - query flags (and --exit) rely on the user doing this
+        return res;          // propagate the result of the tests
+    }
+    return 0;
+}
