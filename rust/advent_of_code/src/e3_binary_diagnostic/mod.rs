b4167247579bbc29eb46196cba7f947e8137f1a1
ChromaMaster <vladtel.dev@gmail.com>
2022-05-08 20:51:47 +0200

    feat: add day three exercise part two in rust

--- rust/advent_of_code/src/e3_binary_diagnostic/mod.rs
+++ rust/advent_of_code/src/e3_binary_diagnostic/mod.rs
@@ -5,17 +5,58 @@ pub fn execute() {
 
     let report: Vec<String> = contents.lines().map(|s| s.to_string()).collect();
 
-    let most_repeated_bits = get_most_repeated_bit(report);
-    let less_repeated_bits = invert_binary_string(&most_repeated_bits);
-
-    let gamma_rate = int_from_binary_string(most_repeated_bits);
-    let epsilon_rate = int_from_binary_string(less_repeated_bits);
 
     println!("--- Part One ---");
+    let gamma_rate = get_gamma_rate(&report);
+    let epsilon_rate = get_epsilon_rate(&report);
     println!("The power consumption of the submarine is: {}", calculate_power_consumption(gamma_rate, epsilon_rate));
+
+    println!("--- Part Two ---");
+    let oxygen_generator_rating = get_oxygen_generator_rating(&report);
+    let get_co2_scrubber_rating = get_co2_scrubber_rating(&report);
+    println!("The life support of the submarine is: {}", calculate_life_support(oxygen_generator_rating, get_co2_scrubber_rating));
+
 }
 
-fn get_most_repeated_bit(input: Vec<String>) -> String {
+fn calculate_power_consumption(gamma_rate: u32, epsilon_rate: u32) -> u32 {
+    gamma_rate * epsilon_rate
+}
+
+fn calculate_life_support(oxygen_generator_rating: u32, co2_scrubber_rating: u32) -> u32{
+    oxygen_generator_rating * co2_scrubber_rating
+}
+
+fn get_gamma_rate(input: &Vec<String>) -> u32 {
+    int_from_binary_string(&get_most_repeated_bit(input))
+}
+
+fn get_epsilon_rate(input: &Vec<String>) -> u32 {
+    int_from_binary_string(&invert_binary_string(&get_most_repeated_bit(input)))
+}
+
+fn get_oxygen_generator_rating(input: &Vec<String>) -> u32 {
+    let mut tmp = input.to_vec();
+    for (i, _) in input.iter().enumerate() {
+        tmp = filter_by_most_common_bit(&tmp, i);
+        if tmp.len() == 1{
+            break
+        }
+    }
+    int_from_binary_string(&tmp[0])
+}
+
+fn get_co2_scrubber_rating(input: &Vec<String>) -> u32 {
+    let mut tmp = input.to_vec();
+    for (i, _) in input.iter().enumerate() {
+        tmp = filter_by_less_common_bit(&tmp, i);
+        if tmp.len() == 1{
+            break
+        }
+    }
+    int_from_binary_string(&tmp[0])
+}
+
+fn get_most_repeated_bit(input: &Vec<String>) -> String {
     let mut tmp = vec![0; input[0].len()];
 
     input.iter().for_each(|value| {
@@ -29,7 +70,7 @@ fn get_most_repeated_bit(input: Vec<String>) -> String {
     });
 
     return tmp.iter().map(|&x| {
-        if x > 0 {
+        if x >= 0 {
             String::from("1")
         } else {
             String::from("0")
@@ -37,22 +78,35 @@ fn get_most_repeated_bit(input: Vec<String>) -> String {
     }).collect();
 }
 
-fn int_from_binary_string(input: String) -> u32 {
+fn int_from_binary_string(input: &String) -> u32 {
     return u32::from_str_radix(input.as_str(), 2).expect("Not a binary number");
-}
-
-fn calculate_power_consumption(gamma_rate: u32, epsilon_rate: u32) -> u32 {
-    return gamma_rate * epsilon_rate;
 }
 
 fn invert_binary_string(binary_string: &String) -> String {
     binary_string.chars().map(|c| {
-        return if c == '0' {
+        if c == '0' {
             '1'
         } else {
             '0'
-        };
+        }
     }).collect()
+}
+
+fn filter_by_same_bit_in_a_position(input: &Vec<String>, mask: String, pos: usize) -> Vec<String> {
+    let mut tmp = input.to_vec();
+    // tmp = tmp.iter().filter(|&s| s.chars().nth(pos).unwrap() == mask.chars().nth(pos).unwrap()).collect();
+    tmp.retain(|s| s.chars().nth(pos).unwrap() == mask.chars().nth(pos).unwrap());
+    return tmp.to_vec();
+}
+
+fn filter_by_most_common_bit(input: &Vec<String>, pos: usize) -> Vec<String> {
+    let mcb = get_most_repeated_bit(&input);
+    filter_by_same_bit_in_a_position(input, mcb, pos)
+}
+
+fn filter_by_less_common_bit(input: &Vec<String>, pos: usize) -> Vec<String> {
+    let lcb = invert_binary_string(&get_most_repeated_bit(&input));
+    filter_by_same_bit_in_a_position(input, lcb, pos)
 }
 
 #[cfg(test)]
@@ -66,13 +120,13 @@ mod tests {
             .map(|s| s.to_string())
             .collect();
 
-        let most_repeated_bits = get_most_repeated_bit(input);
+        let most_repeated_bits = get_most_repeated_bit(&input);
         assert_eq!(most_repeated_bits, String::from("10110"))
     }
 
     #[test]
     fn a_binary_string_is_correctly_converted_to_uint() {
-        assert_eq!(int_from_binary_string(String::from("01100")), 12)
+        assert_eq!(int_from_binary_string(&String::from("01100")), 12)
     }
 
     #[test]
@@ -86,5 +140,47 @@ mod tests {
     fn binary_string_can_be_inverted() {
         let binary_string = String::from("01010");
         assert_eq!(invert_binary_string(&binary_string), String::from("10101"))
+    }
+
+    #[test]
+    fn filter_binary_numbers_by_most_common_bit_in_a_certain_position() {
+        let input = vec!["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        let filtered = filter_by_most_common_bit(&input, 0);
+        assert_eq!(filtered, vec!["11110", "10110", "10111", "10101", "11100", "10000", "11001"])
+    }
+
+    #[test]
+    fn filter_binary_numbers_by_less_common_bit_in_a_certain_position() {
+        let input = vec!["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+
+        let filtered = filter_by_less_common_bit(&input, 0);
+        assert_eq!(filtered, vec!["00100", "01111", "00111", "00010", "01010"])
+    }
+
+    #[test]
+    fn oxygen_generator_rating_is_correctly_calculated() {
+        let input = vec!["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+        let oxygen_generator_rating = get_oxygen_generator_rating(&input);
+        assert_eq!(oxygen_generator_rating, 23);
+    }
+
+    #[test]
+    fn co2_scrubber_rating_is_correctly_calculated() {
+        let input = vec!["00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"]
+            .iter()
+            .map(|s| s.to_string())
+            .collect();
+        let oxygen_generator_rating = get_co2_scrubber_rating(&input);
+        assert_eq!(oxygen_generator_rating, 10);
     }
 }
