9725834835673deabf6a0ad04e98657f66e4dd10
ChromaMaster <vladtel.dev@gmail.com>
2022-12-02 17:57:25 +0100

    feat: add day 2 part one solution

---
+++ 2022/rust/advent_of_code/src/day2/src/game.rs
@@ -0,0 +1,94 @@
+use std::cmp::Ordering;
+use std::cmp::Ordering::{Equal, Greater, Less};
+
+#[derive(PartialEq, Eq, Debug)]
+pub enum Hand {
+    Rock,
+    Paper,
+    Scissors,
+    Unknown(String),
+}
+
+impl From<char> for Hand {
+    fn from(chr: char) -> Self {
+        match chr {
+            'A' | 'X' => Hand::Rock,
+            'B' | 'Y' => Hand::Paper,
+            'C' | 'Z' => Hand::Scissors,
+            _ => Hand::Unknown(format!("{}", chr))
+        }
+    }
+}
+
+impl PartialOrd for Hand {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match self {
+            Hand::Rock => {
+                match other {
+                    Hand::Rock => Some(Equal),
+                    Hand::Paper => Some(Less),
+                    Hand::Scissors => Some(Greater),
+                    _ => None
+                }
+            }
+            Hand::Paper => {
+                match other {
+                    Hand::Rock => Some(Greater),
+                    Hand::Paper => Some(Equal),
+                    Hand::Scissors => Some(Less),
+                    _ => None
+                }
+            }
+            Hand::Scissors => {
+                match other {
+                    Hand::Rock => Some(Less),
+                    Hand::Paper => Some(Greater),
+                    Hand::Scissors => Some(Equal),
+                    _ => None
+                }
+            }
+            _ => None
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hand_is_identified() {
+        let hand = Hand::from('A');
+        assert_eq!(hand, Hand::Rock);
+
+        let hand = Hand::from('B');
+        assert_eq!(hand, Hand::Paper);
+
+        let hand = Hand::from('C');
+        assert_eq!(hand, Hand::Scissors);
+
+        let hand = Hand::from('X');
+        assert_eq!(hand, Hand::Rock);
+
+        let hand = Hand::from('Y');
+        assert_eq!(hand, Hand::Paper);
+
+        let hand = Hand::from('Z');
+        assert_eq!(hand, Hand::Scissors);
+    }
+
+    #[test]
+    fn game_rules_are_applied() {
+        assert_eq!(Hand::Rock, Hand::Rock);
+        assert!(Hand::Rock < Hand::Paper);
+        assert!(Hand::Rock > Hand::Scissors);
+
+        assert_eq!(Hand::Paper, Hand::Paper);
+        assert!(Hand::Paper < Hand::Scissors);
+        assert!(Hand::Paper > Hand::Rock);
+
+        assert_eq!(Hand::Scissors, Hand::Scissors);
+        assert!(Hand::Scissors < Hand::Rock);
+        assert!(Hand::Scissors > Hand::Paper);
+    }
+}
--- 2022/rust/advent_of_code/src/day2/src/main.rs
+++ 2022/rust/advent_of_code/src/day2/src/main.rs
@@ -1,9 +1,55 @@
-fn main() {}
+pub mod game;
+
+use std::fs;
+use game::*;
+
+const SCORE_IF_DRAW: u32 = 3;
+const SCORE_IF_WIN: u32 = 6;
+const SCORE_IF_LOSE: u32 = 0;
+
+fn main() {
+    let input = fs::read_to_string("src/day2/input/input.txt")
+        .expect("Cannot open input file");
+
+    let games = input.trim().split('\n').map(|game| (game, evaluate_game(game)))
+        .collect::<Vec<(&str, u32)>>();
+
+    let total_score = games.iter().fold(0, |acc, game| acc + game.1);
+    println!("Part one: Total score is: {}", total_score)
+}
+
+fn evaluate_game(game: &str) -> u32 {
+    let g = game.split(' ')
+        .map(|hand| Hand::from(hand.chars().collect::<Vec<char>>()[0]))
+        .collect::<Vec<Hand>>();
+
+    let (hand1, hand2) = (&g[0], &g[1]);
+
+    let mut score = match hand2 {
+        Hand::Rock => 1,
+        Hand::Paper => 2,
+        Hand::Scissors => 3,
+        _ => 0
+    };
+
+    score += if hand1 == hand2 { SCORE_IF_DRAW } else if hand2 > hand1 { SCORE_IF_WIN } else { SCORE_IF_LOSE };
+
+    score
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test() {}
+    fn game_score_is_calculated() {
+        let score = evaluate_game("A Y");
+        assert_eq!(score, 8);
+
+        let score = evaluate_game("B X");
+        assert_eq!(score, 1);
+
+        let score = evaluate_game("C Z");
+        assert_eq!(score, 6);
+    }
 }
