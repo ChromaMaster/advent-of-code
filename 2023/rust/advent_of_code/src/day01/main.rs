1c0fd15fb25f34b758488da230e09b366d60d745
ChromaMaster <vladtel.dev@gmail.com>
2023-12-01 15:58:38 +0100

    feat(day01): add solution for part two
    Signed-off-by: Vlad Alexandru Teletin <vladtel.dev@gmail.com>

--- 2023/rust/advent_of_code/src/day01/main.rs
+++ 2023/rust/advent_of_code/src/day01/main.rs
@@ -2,9 +2,14 @@ use advent_of_code::day01;
 
 fn main() {
     let input = advent_of_code::read_input_file("input01.txt");
-    let result = day01::run(input);
-    println!("Result: {}", result);
-    advent_of_code::write_output_file("output01.txt", &result);
+
+    let result = day01::run(&input, false);
+    println!("Result part one: {}", result);
+    advent_of_code::write_output_file("output01a.txt", &result);
+
+    let result = day01::run(&input, true);
+    println!("Result part two: {}", result);
+    advent_of_code::write_output_file("output01b.txt", &result);
 }
 
 #[cfg(test)]
